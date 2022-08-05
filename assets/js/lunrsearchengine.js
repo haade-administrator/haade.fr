0eeee0f64349bf492e3dfe1a0e32f5bfdde20939
haade-administrator <76404800+haade-administrator@users.noreply.github.com>
2022-08-05 15:08:27 +0000

    Deploying to gh-pages from @ haade-administrator/futurofhaade@4dbd726994348b265d75970dc77d7b93dbf4246e 🚀

---
+++ assets/js/lunrsearchengine.js
@@ -0,0 +1,329 @@
+
+var documents = [{
+    "id": 0,
+    "url": "https://haade-administrator.github.io/futurofhaade/fr/blog/integrer-la-teleinfo-au-dashboard-energie-dans-home-assistant",
+    "title": "La téléinfo dans le dashboard Energie de Home Assistant",
+    "description": "La téléinfo dans le dashboard Energie de Home Assistant",
+    "tags": "téléinformation"
+    
+    }, {
+    "id": 1,
+    "url": "https://haade-administrator.github.io/futurofhaade/fr/blog/test-du-capteur-de-luminosite-intelligent-zss-zk-thl-zigbee-moes",
+    "title": "Test - ZSS-ZK-THL zigbee MOES",
+    "description": "Test du capteur de luminosité intelligent ZSS-ZK-THL zigbee MOES dans Homeassistant",
+    "tags": "Test - Zigbee - Homeassistant"
+    
+    }, {
+    "id": 2,
+    "url": "https://haade-administrator.github.io/futurofhaade/en/blog/box-domotique-rail-din",
+    "title": "Domotical Box Rail Din Home-assistant and Raspberrypi",
+    "description": "",
+    "tags": ""
+    
+    }, {
+    "id": 3,
+    "url": "https://haade-administrator.github.io/futurofhaade/fr/blog/box-domotique-rail-din",
+    "title": "Box Domotique rail din Home-assistant et Rpi4",
+    "description": "Création et montage d'une box intégrer au tableau électrique sur rail-din à base de Raspberry4 et Homeassistant",
+    "tags": "Test - Raspberry - Homeassistant"
+    
+    }, {
+    "id": 4,
+    "url": "https://haade-administrator.github.io/futurofhaade/fr/blog/home-assistant-amber-vs-jeedom-atlas",
+    "title": "Home-assistant Amber VS Jeedom Atlas",
+    "description": "Comparatif des deux nouvelles box domotique du moment Homeassistant Amber et Jeedom Atlas",
+    "tags": "Comparatif - Homeassistant - Jeedom"
+    
+    }, {
+    "id": 5,
+    "url": "https://haade-administrator.github.io/futurofhaade/en/blog/home-assistant-amber-vs-jeedom-atlas",
+    "title": "Home-Assistant Amber VS Jeedom Atlas",
+    "description": "",
+    "tags": ""
+    
+    }, {
+    "id": 6,
+    "url": "https://haade-administrator.github.io/futurofhaade/fr/blog/zoneminder-home-assistant-et-machinelearning",
+    "title": "Zoneminder, home assistant et Machinelearning",
+    "description": "Intégration du système de gestion de caméra Zoneminder dans homeassistant avece activation du machinelearning",
+    "tags": "Tutoriel - Zoneminder - Homeassistant"
+    
+    }, {
+    "id": 7,
+    "url": "https://haade-administrator.github.io/futurofhaade/en/blog/zoneminder-home-assistant-et-machinelearning",
+    "title": "Zoneminder, home assistant and Machinelearning",
+    "description": "",
+    "tags": ""
+    
+    }, {
+    "id": 8,
+    "url": "https://haade-administrator.github.io/futurofhaade/fr/blog/test-capteur-porte-zigbee-3-0-sonoff-snzb-04",
+    "title": "Test Capteur porte zigbee 3.0 sonoff SNZB-04",
+    "description": "Test d'un capteur de porte Sonoff SNZB-04 zigbee et intégration dans homeassistant",
+    "tags": "Test - Zigbee - Homeassistant"
+    
+    }, {
+    "id": 9,
+    "url": "https://haade-administrator.github.io/futurofhaade/en/blog/test-capteur-porte-zigbee-3-0-sonoff-snzb-04",
+    "title": "Sonoff SNZB-04 Zigbee 3.0 Door Sensor Test",
+    "description": "",
+    "tags": ""
+    
+    }, {
+    "id": 10,
+    "url": "https://haade-administrator.github.io/futurofhaade/fr/blog/changer-licone-dun-capteur-binaire-dans-home-assistant",
+    "title": "Astuce icone home assistant",
+    "description": "Changer l&#39;icone d&#39;un capteur binaire dans home assistant",
+    "tags": "Astuces - Homeassistant"
+    
+    }, {
+    "id": 11,
+    "url": "https://haade-administrator.github.io/futurofhaade/en/blog/changer-licone-dun-capteur-binaire-dans-home-assistant",
+    "title": "home assistant icon tip",
+    "description": "Change the icon of a binary sensor in home assistant",
+    "tags": ""
+    
+    }, {
+    "id": 12,
+    "url": "https://haade-administrator.github.io/futurofhaade/fr/blog/test-capteur-temperature-et-humidite-sonoff-snzb-02",
+    "title": "Test capteur température et humidité Sonoff SNZB-02",
+    "description": "Test du capteur de tempérture SONOFF SNZB-02 et intégration dans homeassistant",
+    "tags": "Test - Zigbee - Homeassistant"
+    
+    }, {
+    "id": 13,
+    "url": "https://haade-administrator.github.io/futurofhaade/en/blog/test-capteur-temperature-et-humidite-sonoff-snzb-02",
+    "title": "Sonoff SNZB-02 temperature and humidity sensor test",
+    "description": "",
+    "tags": ""
+    
+    }, {
+    "id": 14,
+    "url": "https://haade-administrator.github.io/futurofhaade/fr/blog/z-wave-face-a-matter",
+    "title": "Z-wave face à Matter",
+    "description": "Après le comparatif du z-wave face à zigbee, voici un comparatif face à Matter",
+    "tags": ""
+    
+    }, {
+    "id": 15,
+    "url": "https://haade-administrator.github.io/futurofhaade/en/blog/z-wave-face-a-matter",
+    "title": "Z-wave VS Matter",
+    "description": "",
+    "tags": ""
+    
+    }, {
+    "id": 16,
+    "url": "https://haade-administrator.github.io/futurofhaade/fr/blog/nas-opensource-pour-raspberry-pi-cm4-le-futur",
+    "title": "NAS opensource pour Raspberry pi CM4 le futur!",
+    "description": "Depuis la sortie du CM4 par Raspberry des projets sortent de terre à foison, ici découverte d'un NAS opensource",
+    "tags": "Raspberry - CM4 - Projets"
+    
+    }, {
+    "id": 17,
+    "url": "https://haade-administrator.github.io/futurofhaade/en/blog/nas-opensource-pour-raspberry-pi-cm4-le-futur",
+    "title": "Open source NAS for Raspberry pi CM4 the future!",
+    "description": "",
+    "tags": ""
+    
+    }, {
+    "id": 18,
+    "url": "https://haade-administrator.github.io/futurofhaade/fr/blog/kobol-helios-64-cest-termine",
+    "title": "Kobol helios 64 c'est terminé !",
+    "description": "l'aventure Helios le NAS à faible coût est bel et bien terminé dommage!",
+    "tags": ""
+    
+    }, {
+    "id": 19,
+    "url": "https://haade-administrator.github.io/futurofhaade/en/blog/kobol-helios-64-cest-termine",
+    "title": "Kobol helios 64 is done!",
+    "description": "",
+    "tags": ""
+    
+    }, {
+    "id": 20,
+    "url": "https://haade-administrator.github.io/futurofhaade/fr/blog/test-et-install-thermostat-zigbee-moes-bht-002",
+    "title": "[TEST] et [INSTALL] Thermostat Zigbee Moes BHT-002",
+    "description": "test d'un thermostat zigbee par Moes le BHT-002",
+    "tags": ""
+    
+    }, {
+    "id": 21,
+    "url": "https://haade-administrator.github.io/futurofhaade/en/blog/test-et-install-thermostat-zigbee-moes-bht-002",
+    "title": "[TEST] and [INSTALL] Zigbee Thermostat Moes BHT-002",
+    "description": "",
+    "tags": ""
+    
+    }, {
+    "id": 22,
+    "url": "https://haade-administrator.github.io/futurofhaade/fr/blog/integrer-son-compteur-electrique-teleinfo-dans-home-assistant",
+    "title": "Intégrer son compteur électrique ( téléinfo ) dans Home Assistant",
+    "description": "téléinformation dans homeassistant comment intégrer son compteur edf",
+    "tags": ""
+    
+    }, {
+    "id": 23,
+    "url": "https://haade-administrator.github.io/futurofhaade/en/blog/integrer-son-compteur-electrique-teleinfo-dans-home-assistant",
+    "title": "Integrate your electricity meter (teleinfo) in Home Assistant",
+    "description": "",
+    "tags": ""
+    
+    }, {
+    "id": 24,
+    "url": "https://haade-administrator.github.io/futurofhaade/fr/blog/test-et-install-grow-r503-capteur-dempreintes-digital-avec-esphome-et-homeassistant",
+    "title": "Grow R503 Capteur d&#8217;empreintes digital",
+    "description": "[TEST] et [INSTALL] Grow R503 Capteur d&#8217;empreintes digital avec esphome et homeassistant",
+    "tags": "esphome - homeassistant"
+    
+    }, {
+    "id": 25,
+    "url": "https://haade-administrator.github.io/futurofhaade/fr/blog/frp-configurer-acces-distance-homeassistant",
+    "title": "Frp &#8211; accès à distance pour homeassistant.",
+    "description": "Configurer simplement un accès à distance sécurisé et gratuit avec Home assistant et notre module frp",
+    "tags": "frp - module"
+    
+    }, {
+    "id": 26,
+    "url": "https://haade-administrator.github.io/futurofhaade/fr/blog/augmenter-la-reception-du-signal-wifi-esp8266-esp-12f",
+    "title": "Augmenter le signal wifi sur esp8266 esp-12f esp-12s",
+    "description": "Amélioration du signal wifi d'un module esp8266/esp12 en soudant facilement une antenne externe",
+    "tags": "esp8266 - esp12 - amélioration"
+    
+    }, {
+    "id": 27,
+    "url": "https://haade-administrator.github.io/futurofhaade/fr/blog/raccourci-automatisations-dans-homeassistant-lovelace",
+    "title": "Raccourci automatisations dans Homeassistant Lovelace",
+    "description": "création d'une entité automatisation dans home assistant et lovelace",
+    "tags": "entité - Home-Assistant - lovelace"
+    
+    }, {
+    "id": 28,
+    "url": "https://haade-administrator.github.io/futurofhaade/fr/blog/domotiser-sa-porte-de-garage-pour-5e",
+    "title": "Domotiser sa porte de garage pour 5€",
+    "description": "une façon simple de domotiser sa porte de garage à moindre coût grâce à un module esp8266",
+    "tags": "esp8266"
+    
+    }, {
+    "id": 29,
+    "url": "https://haade-administrator.github.io/futurofhaade/en/blog/domotiser-sa-porte-de-garage-pour-5e",
+    "title": "Automate your garage door for 5€",
+    "description": "a simple way to automate your garage door at a lower cost",
+    "tags": ""
+    
+    }, {
+    "id": 30,
+    "url": "https://haade-administrator.github.io/futurofhaade/fr/blog/esphome-et-homeassistant",
+    "title": "Intégrations Esphome et Homeassistant",
+    "description": "Découverte d'esphome dans homeassistant",
+    "tags": ""
+    
+    }, {
+    "id": 31,
+    "url": "https://haade-administrator.github.io/futurofhaade/fr/blog/test-moes-interrupteur-zigbee-tactile-zts-eu-upgrade-2020",
+    "title": "[TEST] Moes: Interrupteur Zigbee tactile {ZTS-EU} UPGRADE 2020",
+    "description": "Test de l'interrupteur tactile Moes référence zts-eu",
+    "tags": ""
+    
+    }, {
+    "id": 32,
+    "url": "https://haade-administrator.github.io/futurofhaade/fr/blog/protocole-connecte-home-over-ip",
+    "title": "Protocole connecté Home over IP",
+    "description": "Actu du futur protocole HomeoverIP nommé Matter",
+    "tags": ""
+    
+    }, {
+    "id": 33,
+    "url": "https://haade-administrator.github.io/futurofhaade/fr/blog/zigbee-vs-z-wave",
+    "title": "Zigbee vs Z-wave",
+    "description": "Comparons les deux protocoles domotique les plus utilisés z-wave face à zigbee",
+    "tags": "z-wave - zigbee - comparaison"
+    
+    }, {
+    "id": 34,
+    "url": "https://haade-administrator.github.io/futurofhaade/fr/blog/zigbee-comprendre-le-protocole-et-le-maillage",
+    "title": "Zigbee &#8211; comprendre le protocole et le maillage",
+    "description": "Zigbee comprendre le protocole et le maillage de ce standard",
+    "tags": ""
+    
+    }, {
+    "id": 35,
+    "url": "https://haade-administrator.github.io/futurofhaade/fr/blog/automatiser-la-mise-a-jour-des-images-docker-grace-a-watchtower",
+    "title": "Automatiser La Mise A Jour Des Images Docker Grace A Watchtower",
+    "description": "Rendre automatique les mises à jour des images docker grâce à watchtower",
+    "tags": ""
+    
+    }, {
+    "id": 36,
+    "url": "https://haade-administrator.github.io/futurofhaade/fr/blog/installation-complete-et-securisee-de-jeedom-sur-docker",
+    "title": "Jeedom sur docker",
+    "description": "OBSOLÈTE! Installation complète et sécurisée de Jeedom sur docker",
+    "tags": ""
+    
+    }];
+
+var idx = lunr(function () {
+    this.ref('id')
+    this.field('title', { boost: 10 });
+    this.field('description', { boost: 15 });
+    this.field('tags', { boost: 5 });
+
+    documents.forEach(function (doc) {
+        this.add(doc)
+    }, this)
+});
+function lunr_search(term) {
+    document.getElementById('lunrsearchresults').innerHTML = '<ul></ul>';
+    if(term) {
+        document.getElementById('lunrsearchresults').innerHTML = "<p>Résultats pour '" + term + "'</p>" + document.getElementById('lunrsearchresults').innerHTML;
+        //put results on the screen.
+        var results = idx.search(term);
+        if(results.length>0){
+            //console.log(idx.search(term));
+            //if results
+            for (var i = 0; i < results.length; i++) {
+                // more statements
+                var ref = results[i]['ref'];
+                var url = documents[ref]['url'];
+                var title = documents[ref]['title'];
+                var description = documents[ref]['description'];
+                var tags = documents[ref]['tags'];
+                document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML + "<li class='lunrsearchresult'><a href='" + url + "'><span class='title'>" + title + "</span><span class='description'>"+ description +"</span><span class='tags'>"+ tags +"</span><span class='url'>"+ url +"</span></a></li>";
+            }
+        } else {
+            document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = "<li class='lunrsearchresult'>No results found...</li>";
+        }
+    }
+    return false;
+}
+
+function lunr_search(term) {
+    $('#lunrsearchresults').show( 400 );
+
+    document.getElementById('lunrsearchresults').innerHTML = '<div id="resultsmodal" class="modal fade show d-block"  tabindex="-1" role="dialog" aria-labelledby="resultsmodal"> <div class="modal-dialog shadow" role="document"> <div class="modal-content"> <div class="modal-header" id="modtit"> <button type="button" class="close" id="btnx" data-dismiss="modal" aria-label="Close"> &times; </button> </div> <div class="modal-body"> <ul class="mb-0"> </ul>    </div> <div class="modal-footer"><button id="btnx" type="button" class="btn btn-primary btn-sm" data-dismiss="modal">Fermer</button></div></div> </div></div>';
+    if(term) {
+        document.getElementById('modtit').innerHTML = "<h5 class='modal-title'>Résultats pour '" + term + "'</h5>" + document.getElementById('modtit').innerHTML;
+        //put results on the screen.
+        var results = idx.search(term);
+        if(results.length>0){
+            //console.log(idx.search(term));
+            //if results
+            for (var i = 0; i < results.length; i++) {
+                // more statements
+                var ref = results[i]['ref'];
+                var url = documents[ref]['url'];
+                var title = documents[ref]['title'];
+                var description = documents[ref]['description'];
+                var tags = documents[ref]['tags'];
+                document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML + "<li class='lunrsearchresult'><a href='" + url + "'><span class='title'>" + title + "</span><span class='description'>"+ description +"</span><span class='tags'>"+ tags +"</span><small><span class='url'>"+ url +"</span></small></a></li>";
+            }
+        } else {
+            document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = "<li class='lunrsearchresult'>Désolé, aucun résultat trouvé. Fermez et essayez une autre recherche !</li>";
+        }
+    }
+    return false;
+}
+
+$(function() {
+    $("#lunrsearchresults").on('click', '#btnx', function () {
+        $('#lunrsearchresults').hide( 5 );
+    });
+});
