5c375e6346e0a6fd9c67edc99fddfcc6531b7118
Rodriguez <administrateur@haade.fr>
2022-09-04 15:31:51 +0200

    update script scraper

---
+++ assets/js/aliexpresscraper.js
@@ -0,0 +1,20 @@
+const scrape = require('../../node_modules/aliexpress-product-scraper/index.js');
+const item = '32958933105'
+const product = scrape (item);
+const writer = '../../_data/scraper/' + item + ".json"
+const fs = require('fs');
+
+
+product.then(res => {
+  console.log('The JSON: ', res);
+// convert JSON object to string
+const data = JSON.stringify (res);
+
+// write JSON string to a file
+fs.writeFile(writer, data, (err) => {
+    if (err) {
+        throw err;
+    }
+    console.log("JSON data is saved.");
+});
+});
