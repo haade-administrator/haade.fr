326fedc572167c3a66e9642d8e2b28c8590feddd
haade-administrator <76404800+haade-administrator@users.noreply.github.com>
2022-09-25 20:52:18 +0000

    Deploying to gh-pages from @ haade-administrator/haade.fr@daf6faea6dbecbc5849c32552bfbfcc97bc16af1 🚀

--- assets/js/lunrsearchengine-en.js
+++ assets/js/lunrsearchengine-en.js
@@ -1,93 +1,93 @@
 
 var documents = [{
-    "id":0,
+    "id": 0,
     "url": "/haade.fr/en/blog/sonoff-nspanel-smart-scene-wall-switch-smart-wifi-thermostat-temperature",
     "title": "Sonoff NSPanel Smart Scene Wall Switch Smart Wifi Thermostat Temperature",
-    "description": "2022/09/16 -",
+    "description": "2022/09/16 - ",
     "tags": ""
     },{
-    "id":1,
+    "id": 1,
     "url": "/haade.fr/en/blog/controlleur-ouverture-sonoff-snzb-04-zigbee",
     "title": "sonoff snzb-04 zigbee aperture controller",
-    "description": "2022/08/31 -",
+    "description": "2022/08/31 - ",
     "tags": ""
     },{
-    "id":2,
+    "id": 2,
     "url": "/haade.fr/en/blog/box-domotical-box-din-rail",
     "title": "Domotical Box Rail Din Home-assistant and Raspberrypi",
-    "description": "2021/10/05 -",
+    "description": "2021/10/05 - ",
     "tags": ""
     },{
-    "id":3,
+    "id": 3,
     "url": "/haade.fr/en/blog/home-assistant-amber-vs-jeedom-atlas",
     "title": "Home-Assistant Amber VS Jeedom Atlas",
-    "description": "2021/10/01 -",
+    "description": "2021/10/01 - ",
     "tags": ""
     },{
-    "id":4,
+    "id": 4,
     "url": "/haade.fr/en/blog/zoneminder-home-assistant-and-machinelearning",
     "title": "Zoneminder, home assistant and Machinelearning",
-    "description": "2021/09/25 -",
+    "description": "2021/09/25 - ",
     "tags": ""
     },{
-    "id":5,
+    "id": 5,
     "url": "/haade.fr/en/blog/sensor-test-door-zigbee-3-0-sonoff-snzb-04",
     "title": "Sonoff SNZB-04 Zigbee 3.0 Door Sensor Test",
-    "description": "2021/09/20 -",
+    "description": "2021/09/20 - ",
     "tags": ""
     },{
-    "id":6,
+    "id": 6,
     "url": "/haade.fr/en/blog/change-icon-of-a-binary-sensor-in-home-assistant",
     "title": "home assistant icon tip",
-    "description": "2021/09/15 -Change the icon of a binary sensor in home assistant",
+    "description": "2021/09/15 - Change the icon of a binary sensor in home assistant",
     "tags": ""
     },{
-    "id":7,
+    "id": 7,
     "url": "/haade.fr/en/blog/test-sensor-temperature-and-humidity-sonoff-snzb-02",
     "title": "Sonoff SNZB-02 temperature and humidity sensor test",
-    "description": "2021/09/10 -SONOFF SNZB-02 temperature sensor test and integration in homeassistant",
+    "description": "2021/09/10 - SONOFF SNZB-02 temperature sensor test and integration in homeassistant",
     "tags": "Test - Zigbee - Home-assistant"
     },{
-    "id":8,
+    "id": 8,
     "url": "/haade.fr/en/blog/z-wave-vs-matter",
     "title": "Z-wave VS Matter",
-    "description": "2021/09/05 -",
+    "description": "2021/09/05 - ",
     "tags": ""
     },{
-    "id":9,
+    "id": 9,
     "url": "/haade.fr/en/blog/nas-opensource-for-raspberry-pi-cm4-the-future",
     "title": "Open source NAS for Raspberry pi CM4 the future!",
-    "description": "2021/09/01 -",
+    "description": "2021/09/01 - ",
     "tags": ""
     },{
-    "id":10,
+    "id": 10,
     "url": "/haade.fr/en/blog/kobol-helios-64-its-over",
     "title": "Kobol helios 64 is done!",
-    "description": "2021/08/26 -",
+    "description": "2021/08/26 - ",
     "tags": ""
     },{
-    "id":11,
+    "id": 11,
     "url": "/haade.fr/en/blog/temperature-sensor-sonoff-snzb-02-zigbee",
     "title": "Temperature sensor sonoff snzb-02 zigbee",
-    "description": "2021/08/25 -",
+    "description": "2021/08/25 - ",
     "tags": ""
     },{
-    "id":12,
+    "id": 12,
     "url": "/haade.fr/en/blog/test-and-install-thermostat-zigbee-moes-bht-002",
     "title": "[TEST] and [INSTALL] Zigbee Thermostat Moes BHT-002",
-    "description": "2021/08/12 -",
+    "description": "2021/08/12 - ",
     "tags": ""
     },{
-    "id":13,
+    "id": 13,
     "url": "/haade.fr/en/blog/integrate-your-electricity-meter-teleinfo-into-home-assistant",
     "title": "Integrate your electricity meter (teleinfo) in Home Assistant",
-    "description": "2021/08/03 -",
+    "description": "2021/08/03 - ",
     "tags": ""
     },{
-    "id":14,
+    "id": 14,
     "url": "/haade.fr/en/blog/domotize-your-garage-door-for-5",
     "title": "Automate your garage door for 5€",
-    "description": "2021/05/19 -a simple way to automate your garage door at a lower cost",
+    "description": "2021/05/19 - a simple way to automate your garage door at a lower cost",
     "tags": ""
     }];
 
