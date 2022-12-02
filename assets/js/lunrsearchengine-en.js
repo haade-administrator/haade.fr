d0498ffb18220e130e7f4cbd3567625e9791642f
haade-administrator <haade-administrator@users.noreply.github.com>
2022-12-02 03:06:15 +0000

    Deploying to gh-pages from @ haade-administrator/haade.fr@1576c3af2113fc642f7b7ad6dbee09af4df84c3f 🚀

--- assets/js/lunrsearchengine-en.js
+++ assets/js/lunrsearchengine-en.js
@@ -1,280 +1,285 @@
 
 var documents = [{
     "id": 0,
+    "url": "/en/blog/interference-zigbee-wifi-2-4ghz-to-know",
+    "title": "interference between zigbee and wifi frequency 2.4ghz",
+    "description": "30/11/2022 - to take into account, the zigbee and the wifi are calibrated on the 2.4ghz frequency, some advice to take into account when setting the channels in order to improve the quality of the mesh",
+    "tags": ""},{
+    "id": 1,
     "url": "/en/blog/zb-dongle-p-vs-zb-dongle-e-zigbee-sonoff",
     "title": "ZBDongle-P VS ZBDongle-E",
     "description": "20/11/2022 - Comparison of zigbee sonoff dongle-p and dongle-e keys which SOC to choose for the future of protocol management",
     "tags": ""},{
-    "id": 1,
+    "id": 2,
     "url": "/en/blog/Explanation-of-Zones-in-ZM",
     "title": "Explanation of zones in Zoneminder",
     "description": "09/11/2022 - Zoneminder integrates zones that allow you to refine false positives",
     "tags": ""},{
-    "id": 2,
+    "id": 3,
     "url": "/en/blog/Utilisation-des-Filtres-dans-ZM",
     "title": "Use of filters in Zoneminder",
     "description": "08/11/2022 - the use of filters in zoneminder is an essential setting so as not to store too many events",
     "tags": ""},{
-    "id": 3,
+    "id": 4,
     "url": "/en/blog/push-button-test-moes-zts-eu-zigbee",
     "title": "Moes Push Button Zigbee Switch Test ZTS-EU",
     "description": "06/11/2022 - After testing the Moes touch switch, for personal reasons I ordered the same push button",
     "tags": "Tests - Moes - Zigbee"},{
-    "id": 4,
+    "id": 5,
     "url": "/en/blog/box-domotical-box-din-rail",
     "title": "Domotical Box Rail Din Home-assistant and Raspberrypi",
     "description": "05/10/2021 - ",
     "tags": ""},{
-    "id": 5,
+    "id": 6,
     "url": "/en/blog/home-assistant-amber-vs-jeedom-atlas",
     "title": "Home-Assistant Amber VS Jeedom Atlas",
     "description": "01/10/2021 - ",
     "tags": ""},{
-    "id": 6,
+    "id": 7,
     "url": "/en/blog/zoneminder-home-assistant-and-machinelearning",
     "title": "Zoneminder, home assistant and Machinelearning",
     "description": "25/09/2021 - ",
     "tags": ""},{
-    "id": 7,
+    "id": 8,
     "url": "/en/blog/sensor-test-door-zigbee-3-0-sonoff-snzb-04",
     "title": "Sonoff SNZB-04 Zigbee 3.0 Door Sensor Test",
     "description": "20/09/2021 - ",
     "tags": ""},{
-    "id": 8,
+    "id": 9,
     "url": "/en/blog/change-icon-of-a-binary-sensor-in-home-assistant",
     "title": "home assistant icon tip",
     "description": "15/09/2021 - Change the icon of a binary sensor in home assistant",
     "tags": ""},{
-    "id": 9,
+    "id": 10,
     "url": "/en/blog/test-sensor-temperature-and-humidity-sonoff-snzb-02",
     "title": "Sonoff SNZB-02 temperature and humidity sensor test",
     "description": "10/09/2021 - SONOFF SNZB-02 temperature sensor test and integration in homeassistant",
     "tags": "Test - Zigbee - Home-assistant"},{
-    "id": 10,
+    "id": 11,
     "url": "/en/blog/z-wave-vs-matter",
     "title": "Z-wave VS Matter",
     "description": "05/09/2021 - ",
     "tags": ""},{
-    "id": 11,
+    "id": 12,
     "url": "/en/blog/nas-opensource-for-raspberry-pi-cm4-the-future",
     "title": "Open source NAS for Raspberry pi CM4 the future!",
     "description": "01/09/2021 - ",
     "tags": ""},{
-    "id": 12,
+    "id": 13,
     "url": "/en/blog/kobol-helios-64-its-over",
     "title": "Kobol helios 64 is done!",
     "description": "26/08/2021 - ",
     "tags": ""},{
-    "id": 13,
+    "id": 14,
     "url": "/en/blog/test-and-install-thermostat-zigbee-moes-bht-002",
     "title": "[TEST] and [INSTALL] Zigbee Thermostat Moes BHT-002",
     "description": "12/08/2021 - ",
     "tags": ""},{
-    "id": 14,
+    "id": 15,
     "url": "/en/blog/integrate-your-electricity-meter-teleinfo-into-home-assistant",
     "title": "Integrate your electricity meter (teleinfo) in Home Assistant",
     "description": "03/08/2021 - ",
     "tags": ""},{
-    "id": 15,
+    "id": 16,
     "url": "/en/blog/domotize-your-garage-door-for-5",
     "title": "Automate your garage door for 5€",
     "description": "19/05/2021 - a simple way to automate your garage door at a lower cost",
     "tags": ""},{
-    "id": 16,
+    "id": 17,
     "url": "/en/blog/esphome-and-homeassistant",
     "title": "Esphome and Homeassistant integrations",
     "description": "11/05/2021 - Discovery of esphome in homeassistant",
     "tags": ""},{
-    "id": 17,
+    "id": 18,
     "url": "/en/blog/test-moes-switch-zigbee-tactile-zts-eu-upgrade-2020",
     "title": "[TEST] Moes: Touch Zigbee Switch {ZTS-EU} UPGRADE 2020",
     "description": "17/03/2021 - Moes tact switch test reference zts-eu",
     "tags": ""},{
-    "id": 18,
+    "id": 19,
     "url": "/en/blog/connected-protocol-home-over-ip-matter",
     "title": "Connected Protocol Home over IP/Matter",
     "description": "11/02/2020 - News of the future HomeoverIP protocol named Matter",
     "tags": ""},{
-    "id": 19,
+    "id": 20,
     "url": "/en/blog/zigbee-comprendre-le-protocole-et-le-maillage",
     "title": "Zigbee, comprendre le protocole et le mailing",
     "description": "10/02/2020 - Zigbee comprend le protocole et le mailing de ce standard",
     "tags": ""},{
-        "id": 20,
+        "id": 21,
         "url": "/en/product/square-recessed-box-coswall-placo",
         "title": "Coswall Square Flush Box for placo",
         "description": "18/03/2021 - Square recessed box for Placoplatre ideal for home automation modules",
         "tags": ""
         },{
-        "id": 21,
+        "id": 22,
         "url": "/en/product/zigbee-touch-switch-moes-zts-eu-upgrade-2020",
         "title": "Switch Moes ZTS-EU upgrade 2020",
         "description": "18/03/2021 - Zigbee touch switch Moes ZTS-EU upgrade 2020, 1, 2 or 3 commands",
         "tags": ""
         },{
-        "id": 22,
+        "id": 23,
         "url": "/en/product/disque-dur-kingspec-m-2-nvme-ssd",
         "title": "KingSpec M.2 NVME ssd hard drive",
         "description": "19/03/2021 - quality Nvme hard drive at an unbeatable price",
         "tags": ""
         },{
-        "id": 23,
+        "id": 24,
         "url": "/en/product/carte-dextension-argon-one-m-2",
         "title": "Argon One M.2 expansion card",
         "description": "19/03/2021 - Additional expansion card for the argon + box in order to be able to integrate an NVME hard disk",
         "tags": ""
         },{
-        "id": 24,
+        "id": 25,
         "url": "/en/product/recessed-box-to-seal-coswall-85mm",
         "title": "Junction box Coswall square",
         "description": "19/03/2021 - Square recessed box ideal for home automation module",
         "tags": ""
         },{
-        "id": 25,
+        "id": 26,
         "url": "/en/product/box-alu-raspberrpi-4-argon-with-disk-nvme-m-2",
         "title": "Aluminum box raspberry 4 Argon with Nvme M.2 disk",
         "description": "19/03/2021 - Argon box complete with aluminum and nvme ssd support for raspberry-pi 4",
         "tags": ""
         },{
-        "id": 26,
+        "id": 27,
         "url": "/en/product/upgrade-2020-moes-zigbee-rf-switch",
         "title": "Upgrade 2020 Moes zigbee-rf switch on old",
         "description": "21/03/2021 - ",
         "tags": ""
         },{
-        "id": 27,
+        "id": 28,
         "url": "/en/product/aqara-door-sensor-by-xiaomi-zigbee-3-0",
         "title": "Aqara door sensor by xiaomi zigbee 3.0",
         "description": "25/03/2021 - Door opening sensor Xiaomi Aquara compatible zigbee 3.0",
         "tags": ""
         },{
-        "id": 28,
+        "id": 29,
         "url": "/en/product/thermostat-moes-compatibility-zigbee-3-0",
         "title": "Zigbee 3.0 compatible Moes thermostat",
         "description": "25/03/2021 - Moes touch thermostat with zigbee 3.0 chip",
         "tags": ""
         },{
-        "id": 29,
+        "id": 30,
         "url": "/en/product/camera-colorvu-protocol-hikvision",
         "title": "colorvu camera hikvision protocol",
         "description": "27/03/2021 - ",
         "tags": ""
         },{
-        "id": 30,
+        "id": 31,
         "url": "/en/product/argon-one-first-range-box-for-raspberry-pi-4b",
         "title": "Argon One+ raspberrypi box",
         "description": "27/03/2021 - Quality aluminum case for raspberry pi 4",
         "tags": ""
         },{
-        "id": 31,
+        "id": 32,
         "url": "/en/product/relay-7-30v-esp-8266",
         "title": "ESP8266 relay 7-30v ESP-12F",
         "description": "17/05/2021 - ",
         "tags": ""
         },{
-        "id": 32,
+        "id": 33,
         "url": "/en/product/module-de-conversion-usb-uart-ft232",
         "title": "ft232 converter module",
         "description": "19/05/2021 - usb uart ft232 converter module",
         "tags": ""
         },{
-        "id": 33,
+        "id": 34,
         "url": "/en/product/multiroom-up2stream-amp-v4",
         "title": "Multiroom upstream anp v4",
         "description": "20/05/2021 - ",
         "tags": ""
         },{
-        "id": 34,
+        "id": 35,
         "url": "/en/product/grandir-r503-stainless-steel-plate-plus-protection-r503",
         "title": "GRANDIR R503 stainless steel",
         "description": "16/06/2021 - GRANDIR R503 stainless steel plate plus protection for Grow R503 fingerprint",
         "tags": ""
         },{
-        "id": 35,
+        "id": 36,
         "url": "/en/product/grow-r503-fingerprint-sensor",
         "title": "Grow R503 - Fingerprint sensor",
         "description": "16/06/2021 - ",
         "tags": ""
         },{
-        "id": 36,
+        "id": 37,
         "url": "/en/product/thermometer-zigbee-moes",
         "title": "Zigbee Moes 3 in 1 Thermometer",
         "description": "03/08/2021 - zigbee 3.0 thermometer by Moes",
         "tags": ""
         },{
-        "id": 37,
+        "id": 38,
         "url": "/en/product/temperature-sensor",
         "title": "zigbee digital temperature sensor",
         "description": "09/08/2021 - Zemismart Tuya Zigbee Temperature and Humidity Sensor with LCD Display Real-time Monitor for Smart Home Smart Link",
         "tags": ""
         },{
-        "id": 38,
+        "id": 39,
         "url": "/en/product/sonoff-zigbee-zbmini",
         "title": "Sonoff zigbee zbmini",
         "description": "23/08/2021 - Sonoff Zbmini control switch and light zigbee 3.0",
         "tags": ""
         },{
-        "id": 39,
+        "id": 40,
         "url": "/en/product/temperature-sensor-sonoff-snzb-02-zigbee",
         "title": "Temperature sensor sonoff snzb-02 zigbee",
         "description": "25/08/2021 - ",
         "tags": ""
         },{
-        "id": 40,
+        "id": 41,
         "url": "/en/product/touch-switch-z-wave-neo-coolcam",
         "title": "z-wave neo coolcam touch switch",
         "description": "25/08/2021 - Z-wave switch glass effect range neo coolcam well placed quality-price",
         "tags": "z-wave"
         },{
-        "id": 41,
+        "id": 42,
         "url": "/en/product/fan-for-raspberry-pi4",
         "title": "Fan for Raspberrypi 4",
         "description": "06/10/2021 - The raspberry tends to heat up this fan will do what is necessary not to exceed 50°C",
         "tags": "raspberrypi - accessories"
         },{
-        "id": 42,
+        "id": 43,
         "url": "/en/product/display-lcd-tft-spi",
         "title": "SPI TFT-LCD display",
         "description": "06/10/2021 - Additional screen for raspberrypi type microcomputer",
         "tags": "raspberrypi - accessories"
         },{
-        "id": 43,
+        "id": 44,
         "url": "/en/product/raspberry-pi-touch-screen",
         "title": "Raspberry Pi touch screen",
         "description": "11/10/2021 - 10.1 inch Raspberry Pi Touch Screen RPI 2 3 4 Model B Case EVICIV UPi06 Max Portable Monitor RasPi USB C Touch Display",
         "tags": ""
         },{
-        "id": 44,
+        "id": 45,
         "url": "/en/product/opening-control-sonoff-snzb-04-zigbee",
         "title": "sonoff snzb-04 zigbee aperture controller",
         "description": "31/08/2022 - ",
         "tags": ""
         },{
-        "id": 45,
+        "id": 46,
         "url": "/en/product/wifi-wall-switch-for-intelligent-scene-nspanel",
         "title": "Sonoff NSPANEL",
         "description": "16/09/2022 - wifi wall switch for smart stage NSPanel",
         "tags": ""
         },{
-        "id": 46,
+        "id": 47,
         "url": "/en/product/key-zigbee-3-sonoff-Z-Bridge-dongle-E",
         "title": "zigbee key 3.0 Sonoff zbridge Dongle-E",
         "description": "23/09/2022 - Sonoff dongle compatible zigbee2mqtt and homeassistant based on cc2652 chip",
         "tags": ""
         },{
-        "id": 47,
+        "id": 48,
         "url": "/en/product/inductive-sensor-5v-npn-LJ18A3-8Z",
         "title": "5v inductive sensor LJ18A3-8Z/BX for water meter",
         "description": "29/11/2022 - thanks to this inductive sensor you can easily follow your water consumption on your favorite home automation assistant",
         "tags": ""
         },{
-        "id": 48,
+        "id": 49,
         "url": "/en/product/plate-inductive-sensor-DC-NPN-NO-5VDC",
         "title": "5v flat inductive sensor TL-W5MC1-5V for water meter",
         "description": "29/11/2022 - thanks to this flat inductive sensor you can easily follow your water consumption on your favorite home automation assistant ideal for small spaces",
         "tags": ""
         },{
-        "id": 49,
+        "id": 50,
         "url": "/en/product/wemos-d1-mini-pro-with-antenna",
         "title": "wemos mini D1 pro esp module with antenna",
         "description": "30/11/2022 - Renowned esp8266 development module wemos D1 mini pro with antenna to best capture wifi waves",
