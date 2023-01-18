d60c0687dc3a93b2e00f8f08cb03e3b304c02ae3
nicoxygen <administrateur@haade.fr>
2023-01-18 02:12:26 +0100

    add affiliate link seearch

--- _plugins/affiliate_links.rb
+++ _plugins/affiliate_links.rb
@@ -2,15 +2,15 @@
 module MyLiquidFilters
     # Checks if the post contains amazon affiliate links
     def has_amazon(input)
-      input =~ /(https:\/\/amzn.to|tag=haade0a-21)/i ? true : false
+      input =~ /(https:\/\/amzn.to)/i ? true : false
     end
-    # Checks if the post contains aliexpress affiliate links
+    # # Checks if the post contains aliexpress affiliate links
     def has_aliexpress(input)
       input =~ /(https:\/\/s.click.aliexpress.com)/i ? true : false
     end
-    # Checks if the post contains aliexpress affiliate links
+    # # Checks if the post contains domadoo affiliate links
     def has_domadoo(input)
-      input =~ /(?domid=39)/i ? true : false
+      input =~ /(\?domid=39)/i ? true : false
     end
   
     Liquid::Template.register_filter self
