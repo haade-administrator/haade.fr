90093c99bf3fbb5636da85c924800ab62b63b430
Nicolas <administrateur@haade.fr>
2023-01-21 18:14:04 +0100

    add domadoo scraper

--- _data/domadoo/data.js
+++ _data/domadoo/data.js
@@ -6,5 +6,7 @@ module.exports = [
     'interface-domotique/6315-sonoff-cle-usb-zigbee-30-antenne-externe-20dbm-v2-zbdongle-e',
     'objets-connectes/5985-sonoff-interrupteur-mural-nspanel-smart-scene-eu',
     'peripheriques/6473-neo-prise-intelligente-zigbee-tuya-16a-mesure-de-consommation',
-    'peripheriques/5385-aqara-prise-connectee-zigbee-30-smart-plug-sp-euc01'
+    'peripheriques/5385-aqara-prise-connectee-zigbee-30-smart-plug-sp-euc01',
+    '6165-nous-prise-intelligente-zigbee-30-mesure-de-consommation-5907772033517',
+    'peripheriques/6427-sonoff-interrupteur-mural-connecte-wifi-sur-secteur-2-canaux-m5'
 ]
