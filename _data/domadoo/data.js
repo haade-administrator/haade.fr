e90fc2240d1176a69183144dbdc87c3a4f2ad4e3
Nicolas <administrateur@haade.fr>
2023-02-05 16:23:41 +0100

    update post product

--- _data/domadoo/data.js
+++ _data/domadoo/data.js
@@ -3,10 +3,10 @@ module.exports = [
     'peripheriques/5375-sonoff-micromodule-commutateur-connecte-zigbee-zbmini-6920075776133',
     'interface-domotique/6315-sonoff-cle-usb-zigbee-30-antenne-externe-20dbm-v2-zbdongle-e',
     'objets-connectes/5985-sonoff-interrupteur-mural-nspanel-smart-scene-eu',                       // Sonoff nspanel
-    'peripheriques/6473-neo-prise-intelligente-zigbee-tuya-16a-mesure-de-consommation',
+    'peripheriques/6473-neo-prise-intelligente-zigbee-tuya-16a-mesure-de-consommation',             // Prise Neo coolcam 007SPB2
     'peripheriques/5385-aqara-prise-connectee-zigbee-30-smart-plug-sp-euc01',
-    '6165-nous-prise-intelligente-zigbee-30-mesure-de-consommation-5907772033517',
-    'peripheriques/6427-sonoff-interrupteur-mural-connecte-wifi-sur-secteur-2-canaux-m5',
-    'box-domotique/6280-jeedom-controleur-domotique-jeedom-luna-z-wave-700-et-zigbee-30'           // jeedom luna
-
+    '6165-nous-prise-intelligente-zigbee-30-mesure-de-consommation-5907772033517',                 // Prise Nous A1Z
+    'peripheriques/6352-immax-sirene-intelligente-exterieure-zigbee-30-tuya-ip65',                 // Alarme Sirène immax
+    'box-domotique/6280-jeedom-controleur-domotique-jeedom-luna-z-wave-700-et-zigbee-30',          // jeedom luna
+    'peripheriques/5714-heiman-sirene-interieure-intelligente-zigbee-30'                           // Sirène Heiman
 ]
---
+++ _posts/fr/2023-02-05-test-sirene-immax-neo-07504L-zigbee3-par-domadoo.md
@@ -0,0 +1,29 @@
+---
+guid: 44
+title: "Test de la sirène Immax Neo 07504L zigbee 3.0"
+description: "Test et déballage de la sirène Immax Neo zigbee 3.0 07504L quels osnt ses atouts ?"
+layout: post
+author: Nico
+date: 2023-02-05 11:09
+last_modified_at: 
+categories: [Tests, Haade-lab, Zigbee]
+tags: []
+image: 'picture.png'
+toc: true
+beforetoc: ''
+published: false
+noindex: false
+sitemap:
+  changefreq: 'monthly'
+  exclude: 'no'
+  priority: 0.5 #between 0.0 to 1.0, 1.0 high priority
+  lastmod:  # date to end modification
+locale: fr_FR
+comments: true
+rating:  
+sourcelink:
+  - https://www.immax.eu/immax-neo-smart-siren-zigbee-3-0-p11154/?cid=301
+  - https://www.domadoo.fr/fr/peripheriques/6352-immax-sirene-intelligente-exterieure-zigbee-30-tuya-ip65.html?domid=39
+---
+
+{% include doclink.html pdf="Immax-Neo-Sirene-07504L-manual.pdf" title="Manuel alarme Immax Neo 07504L" %}
