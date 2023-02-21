323f8225d9673d23529d093ee8e9e66fc7467e1b
Nicolas <administrateur@haade.fr>
2023-02-21 11:02:44 +0100

    update minor bugs

--- _plugins/views.rb
+++ _plugins/views.rb
@@ -11,22 +11,24 @@ module Jekyll
 
       return unless countapi_namespace
 
+      error_file = File.new("_errors.log", "w")
+
       site.posts.docs.each do |post|
-        url_encoded = File.join(File.dirname(post.url), File.basename(post.url, ".*")).gsub('/', '-').slice(1, 64) + ".md"
+        url_encoded = post.url.gsub('/', '-').slice(1, 64)
         countapi_url = URI::DEFAULT_PARSER.escape("https://api.countapi.xyz/get/#{countapi_namespace}/#{url_encoded}")
 
         begin
           views = JSON.parse(URI.open(countapi_url).read)['value']
-        rescue OpenURI::HTTPError, JSON::ParserError
+          post.data['views'] = views.to_i
+        rescue OpenURI::HTTPError, JSON::ParserError => e
+          error_file.puts("#{post.url}: #{e.message}")
           next
         end
-
-        post.data['views'] = views.to_i
       end
+
+      error_file.close
 
       site.posts.docs.sort_by! { |post| -post.data['views'].to_i }
     end
   end
 end
-
-
