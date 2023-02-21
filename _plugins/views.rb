f6c37c7bc05784cf91b154c6e63d544681d26656
Nicolas <administrateur@haade.fr>
2023-02-21 15:31:13 +0100

    update views.rb test

--- _plugins/views.rb
+++ _plugins/views.rb
@@ -8,27 +8,27 @@ module Jekyll
 
     def generate(site)
       countapi_namespace = site.config.dig('countapi', 'namespace')
-
       return unless countapi_namespace
 
-      error_file = File.new("_errors.log", "w")
-
+      views_data = {}
       site.posts.docs.each do |post|
-        url_encoded = post.url.gsub('/', '-').slice(1, 64)
-        countapi_url = URI::DEFAULT_PARSER.escape("https://api.countapi.xyz/get/#{countapi_namespace}/#{url_encoded}")
-
+        url_encoded = URI::DEFAULT_PARSER.escape(post.url.gsub('/', '-').slice(1, 64))
+        countapi_url = "https://api.countapi.xyz/get/#{countapi_namespace}/#{url_encoded}"
+        puts "CountAPI URL: #{countapi_url}"
         begin
           views = JSON.parse(URI.open(countapi_url).read)['value']
-          post.data['views'] = views.to_i
+          post.data['views'] = views.to_i || 0
+          puts "Views: #{views}"
+          views_data[post.id] = views.to_i
         rescue OpenURI::HTTPError, JSON::ParserError => e
-          error_file.puts("#{post.url}: #{e.message}")
           next
         end
       end
 
-      error_file.close
-
-      site.posts.docs.sort_by! { |post| -post.data['views'].to_i }
+      # Write views data to file
+      views_file = File.join(site.config['source'], '_data', 'views.json')
+      File.write(views_file, JSON.generate(views_data))
     end
   end
 end
+
