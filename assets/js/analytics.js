b47c4bcde8e35360bade749e86851689594d7085
Nicolas <administrateur@haade.fr>
2023-05-30 16:05:34 +0200

    add product 2162

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -1,16 +1,18 @@
 // add goatcounter integration json with goatcounter.com
 
 (function() {
-    var goatcounterScript = document.createElement('script');
-    goatcounterScript.setAttribute('data-goatcounter', 'https://{{ goatcounter.id }}.goatcounter.com/count');
-    goatcounterScript.setAttribute('async', '');
-    goatcounterScript.src = '//gc.zgo.at/count.js';
-    document.head.appendChild(goatcounterScript);
-  
-    var r = new XMLHttpRequest();
-    r.addEventListener('load', function() {
-      document.getElementById('visits').innerText = JSON.parse(this.responseText).count;
-    });
-    r.open('GET', 'https://haade.goatcounter.com/counter/' + encodeURIComponent(location.pathname) + '.json');
-    r.send();
-  })();
+  var goatcounterScript = document.createElement('script');
+  goatcounterScript.setAttribute('data-goatcounter', 'https://{{ site.goatcounter.id }}.goatcounter.com/count');
+  goatcounterScript.setAttribute('async', '');
+  goatcounterScript.src = '//gc.zgo.at/count.js';
+  document.head.appendChild(goatcounterScript);
+
+  var r = new XMLHttpRequest();
+  r.addEventListener('load', function() {
+    document.getElementById('visits').innerText = JSON.parse(this.responseText).count;
+  });
+  r.open('GET', 'https://{{ site.goatcounter.id }}.goatcounter.com/counter/' + encodeURIComponent(location.pathname) + '.json');
+  r.send();
+})();
+
+// <script src="analytics.js?timestamp={{ "now" | date: "%s" }}"></script>
