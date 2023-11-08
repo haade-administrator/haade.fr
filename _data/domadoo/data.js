7cd7a639484c9b0d90e530a798449230953a7560
haade-administrator <administrateur@haade.fr>
2023-11-08 16:55:41 +0100

    add scraper domadoo

--- _data/domadoo/data.js
+++ _data/domadoo/data.js
@@ -42,5 +42,6 @@ module.exports = [
     'chauffage-clim/6732-sonoff-capteur-de-temperature-et-d-humidite-zigbee-30-avec-support-snzb-02p', // 2172 sonoff snzb-02p
     'produits/6783-sonoff-module-connecte-onoff-matter-10a-minir4m', // 2173 sonoff miniR4M
     'peripheriques/6321-sonoff-box-domotique-zigbee-30-wifi-pro', // 2174 zbbridge pro
-    'peripheriques/5409-frient-detecteur-de-fumee-intelligent-zigbee-30-5713594002330' // 2175 frient CF smszb-120
+    'peripheriques/5409-frient-detecteur-de-fumee-intelligent-zigbee-30-5713594002330', // 2175 frient CF smszb-120
+    'peripheriques/6671-moes-vanne-motorisee-zigbee-tuya' // 2176 vanne motorisée Moes
 ]
