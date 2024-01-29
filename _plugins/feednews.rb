f8ea40bd2e49fc9e9d3304dc9840ce4fdfa595b4
haade-administrator <administrateur@haade.fr>
2024-01-29 10:35:27 +0100

    fix error + multi-lang feednews script

--- _plugins/feednews.rb
+++ _plugins/feednews.rb
@@ -39,34 +39,33 @@ File.open(locale_data_file, 'w') do |file|
   feed.each do |feeditem|
     name = feeditem['name']
     feed_url = feeditem['feed']
-    feed_lang = feeditem['lang'] || 'fr_FR'  # Utiliser 'fr_FR' par défaut si la langue n'est pas spécifiée
+    feed_langs = Array(feeditem['lang'] || 'fr_FR')  # Utiliser 'fr_FR' par défaut si la langue n'est pas spécifiée
 
     rss = FeedNormalizer::FeedNormalizer.parse URI.open(feed_url)
     rss.parser = 'SimpleRSS'
 
     locale_entries = []
-    # Ajouter la condition reverse-time
-    reverse_time = feeditem['reverse-time'] || false
-    # Trier les entrées par date, de la plus récente à la plus ancienne
-    sorted_entries = rss.entries.sort_by { |entry| reverse_time ? -entry.date_published.to_i : entry.date_published.to_i }.reverse
-    # Prendre seulement la première entrée (la plus récente)
-    latest_entry = sorted_entries.first
 
+    # Utiliser la première entrée sans tri par date
+    latest_entry = rss.entries.first
+    
     if latest_entry
       title = latest_entry.title.encode('utf-8', :invalid => :replace, :undef => :replace, :replace => '-').gsub(':', ' -')
       entry_url = latest_entry.urls.first
       description = latest_entry.description || latest_entry.content
-
+    
       # Extraction de l'URL de l'image depuis la description
       image_url = extract_image_from_description(description)
+    
+      feed_langs.each do |feed_lang|
+        locale_entry = {'name' => name, 'title' => title, 'url' => entry_url, 'image' => image_url }
 
-      locale_entry = {'name' => name, 'title' => title, 'url' => entry_url, 'image' => image_url }
-      locale_entries << locale_entry
+        # Utiliser la langue spécifiée dans le fichier d'appel
+        feed_data[name] ||= {}
+        feed_data[name][feed_lang] ||= []
+        feed_data[name][feed_lang] << locale_entry
+      end
     end
-
-    # Utiliser la langue spécifiée dans le fichier d'appel
-    feed_data[name] ||= {}
-    feed_data[name][feed_lang] = locale_entries
   end
 
   # Écriture des données dans le fichier YAML sans les tirets
