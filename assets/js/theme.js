95d2fb2948ee0f1dd81eaf6b21e0f0d5a43cb6d0
haade-administrator <76404800+haade-administrator@users.noreply.github.com>
2024-05-31 11:34:54 +0200

    Deploying to gh-pages from @ haade-administrator/haade.fr@a7ef3ac8ec391f3df35413d1907c9f8d349a1429 🚀

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -209,10 +209,10 @@ document.addEventListener('DOMContentLoaded', () => {
       'fast-forward', // Fast forward by the seek time (default 10 seconds)
       'progress', // The progress bar and scrubber for playback and buffering
       // 'current-time', // The current time of playback
-      // 'duration', // The full duration of the media
+      'duration', // The full duration of the media
       'mute', // Toggle mute
       'volume', // Volume control
-      'captions', // Toggle captions
+      // 'captions', // Toggle captions
       'settings', // Settings menu
       'pip', // Picture-in-picture (currently Safari only)
       'airplay', // Airplay (currently Safari only)
